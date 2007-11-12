2978f579c103f55b34dbfec6adfc1af1c3278de7
psaraci <psaraci@c2d3a650-433d-0410-a83f-3989eb8f22bf>
2007-11-12 23:21:26 +0000

    Initial commits! initial code for the protocol (server-client protocol a bit more than 60% done)

---
+++ trunk/src/globals/networkProtocol.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Petref Saraci                                   *
+ *   psaraci@gmail.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <arpa/inet.h>
+#include <QTcpSocket>
+
+class NetworkProtocol: public QObject
+{
+    Q_OBJECT
+
+    public:
+        NetworkProtocol( QObject *parent = 0, QTcpSocket *sock =0 );
+        ~NetworkProtocol();
+
+        enum ProtocolError { InvalidFormat =0, InvalidVersion = 1 };
+
+
+        QByteArray createPacket( const QString &msg, qint8 type ) const;
+        qint32 sizeOfPacket( const QByteArray &packet ) const
+        {
+            //this is the size of the packet, the byte order is changed from host to network
+            return htonl( packet.size() );
+        }
+
+    public slots:
+        void readData();
+
+    signals:
+        void messageReady( const QString msg);
+        void networkProtocolError( NetworkProtocol::ProtocolError err );
+
+    private:
+        QTcpSocket *client;
+        QString clientError;
+        qint8 messageType;
+        qint32 packetSize;
+        qint32 protocolFormat;
+        qint32 protocolVersion;
+};
